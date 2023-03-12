958e52fd8b31a3146774cd36afca82bcaa2ffb9b
NotKatsu <katsudiscord@outlook.com>
2023-03-12 16:53:25 +0000

    Example added with wordList.

---
+++ examples/check_string.py
@@ -0,0 +1,8 @@
+import censorpy
+
+wordList = ["fuck", "shit", "shat"]
+
+censor = censorpy.censor(words=wordList, remove_zero_width_spaces=True, remove_symbols=True)
+
+print(censor.check("f.u.c.k $hit sh@t"))
+
