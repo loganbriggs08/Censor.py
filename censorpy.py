2bf22a1dcc1c4779dbf86136312cd8bacdabafef
NotKatsu <katsudiscord@outlook.com>
2023-03-12 14:26:18 +0000

    Added Config and the ability to check it

--- censorpy.py
+++ censorpy.py
@@ -1,13 +1,16 @@
+from typing import Union
 from handlers.checkerHandler import checker
 
 class censor:
-    def __init__(self, check_ascii: bool, remove_zero_width_spaces: bool, words: list[str]):
+    def __init__(self, check_ascii: Union[bool, None], remove_zero_width_spaces: Union[bool, None], words: list[str]):
         self.check_ascii = check_ascii
         self.remove_zero_width_spaces = remove_zero_width_spaces
-        self.words = words
+        self.words_list = words
 
-
-    def config(self):
-        print(self.check_ascii, self.remove_zero_width_spaces, self.words)
+    def config(self) -> str:
+        return {"check_ascii": self.check_ascii, "remove_zero_width_spaces": self.remove_zero_width_spaces, "words": self.words_list}
+    
+    def check(self, string: str) -> list[str]:
+        return checker(string, self.check_ascii, self.remove_zero_width_spaces, self.words_list)
 
 
