cd3fcb99297ee7f0c02c0a1fcb650565f9e74558
NotKatsu <katsudiscord@outlook.com>
2023-03-17 23:31:46 +0000

    Censor option now available

--- censorpy.py
+++ censorpy.py
@@ -1,14 +1,15 @@
 from typing import Union
 
 from handlers.checkerHandler import checker
-
+from handlers.censorHandler import censor as censor_handler
 
 class censor:
     def __init__(self, remove_zero_width_spaces: Union[bool, None], remove_symbols: Union[bool, None],
-                 words: list[str]):
+                 words: list[str], change_character: str):
         self.remove_zero_width_spaces = remove_zero_width_spaces
         self.remove_symbols = remove_symbols
         self.words_list = words
+        self.change_character = change_character
 
     def config(self) -> str:
         """Get your configuration for censorpy.
@@ -29,3 +30,15 @@ class censor:
             list (str): list of words that have triggered the checker.
         """
         return checker(string, self.remove_zero_width_spaces, self.remove_symbols, self.words_list)
+    
+    def censor(self, string: str) -> str:
+        """Check a string for words in your wordlist and replace them with your custom char.
+
+        Args:
+            string (str): string that you would like to check and change.
+
+        Returns:
+            str: str with any changed words.
+        """
+        return censor_handler(string, self.remove_zero_width_spaces, self.remove_symbols, self.words_list, self.change_character)
+
